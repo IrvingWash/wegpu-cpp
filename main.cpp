17d19c48f6a7a34198b0dda730ff2dd2d9ca3ff1
Washington Irving <iamwashingyou@gmail.com>
2024-03-03 16:49:09 +0300

    get adatper

--- main.cpp
+++ main.cpp
@@ -1,5 +1,47 @@
 #include "glfw/include/GLFW/glfw3.h"
+#include "dawn/include/webgpu/webgpu.hpp"
+#include "glfw3webgpu/glfw3webgpu.h"
+#include <vector>
 #include <iostream>
+
+WGPUAdapter requestAdapter(WGPUInstance instance, WGPURequestAdapterOptions const* options) {
+    struct UserData {
+        WGPUAdapter adapter = nullptr;
+        bool requestEnded = false;
+    };
+    UserData userData;
+
+    auto onAdapterRequestEnded = [](
+        WGPURequestAdapterStatus status,
+        WGPUAdapter adapter,
+        char const* message,
+        void* pUserData
+    ) {
+        UserData& userData = *reinterpret_cast<UserData*>(pUserData);
+
+        if (status == WGPURequestAdapterStatus_Success) {
+            userData.adapter = adapter;
+        } else {
+            std::cout
+                << "Could not get WebGPU adapter: "
+                << message <<
+                std::endl;
+        }
+
+        userData.requestEnded = true;
+    };
+
+    wgpuInstanceRequestAdapter(
+        instance,
+        options,
+        onAdapterRequestEnded,
+        (void*)&userData
+    );
+
+    assert(userData.requestEnded);
+
+    return userData.adapter;
+}
 
 int main() {
     if (!glfwInit()) {
@@ -7,6 +49,7 @@ int main() {
         return 1;
     }
 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(640, 480, "Learn WebGPU", NULL, NULL);
 
     if (!window) {
@@ -19,6 +62,39 @@ int main() {
         glfwPollEvents();
     }
 
+    WGPUInstanceDescriptor desc = {};
+    desc.nextInChain = nullptr;
+
+    WGPUInstance instance = wgpuCreateInstance(&desc);
+
+    if (!instance) {
+        std::cerr << "Could not initialize WebGPU!" << std::endl;
+        return 1;
+    }
+
+    WGPURequestAdapterOptions options = {};
+    options.nextInChain = nullptr;
+    WGPUSurface surface = glfwGetWGPUSurface(instance, window);
+    options.compatibleSurface = surface;
+
+    WGPUAdapter adapter = requestAdapter(instance, &options);
+
+    std::vector<WGPUFeatureName> features;
+
+    size_t feature_count = wgpuAdapterEnumerateFeatures(adapter, nullptr);
+
+    features.resize(feature_count);
+
+    wgpuAdapterEnumerateFeatures(adapter, features.data());
+
+    std::cout << "Adapter features" << std::endl;
+
+    for (auto feature : features) {
+            std::cout << " - " << feature << std::endl;
+    }
+
+    wgpuSurfaceRelease(surface);
+    wgpuInstanceRelease(instance);
     glfwDestroyWindow(window);
     glfwTerminate();
 }
